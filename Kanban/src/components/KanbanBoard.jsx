f977242a3dd643457b970d7657fa7f08b978cd10
Sthabiso <sthabiso@aims.ac.za>
2024-02-23 09:58:23 +0200

    Created a Kanban board component

---
+++ Kanban/src/components/KanbanBoard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { DragDropContext } from 'react-beautiful-dnd';
+
+
+export default function KanbanBoard(){
+
+
+    return (
+        <DragDropContext>
+
+            
+        </DragDropContext>
+    )
+}
