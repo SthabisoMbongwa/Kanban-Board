b37cd43780be245a6a72eb6ea692c2a04e7de521
Sthabiso <sthabiso@aims.ac.za>
2024-02-23 10:02:41 +0200

    Created two useStates hooks, one for completed task and the other for incomplete tasks

--- Kanban/src/components/KanbanBoard.jsx
+++ Kanban/src/components/KanbanBoard.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { DragDropContext } from 'react-beautiful-dnd';
 
 
 export default function KanbanBoard(){
-
+    const [completed, setCompleted] = useState([]);
+    const [incomplete, setIncomplete] = useState([]);
 
     return (
         <DragDropContext>
+            <h2 style={{ textAlign: "center"}}>PROGRESS BOARD</h2>
 
-            
         </DragDropContext>
     )
 }
