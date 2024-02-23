7da67ffe8a7620fc46f00b91881317eaca4d0c76
Sthabiso <sthabiso@aims.ac.za>
2024-02-23 17:19:21 +0200

    Used useEffect and fetch inside useEffect to get data from jsonplaceholder

--- Kanban/src/components/KanbanBoard.jsx
+++ Kanban/src/components/KanbanBoard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { DragDropContext } from 'react-beautiful-dnd';
 import Column from './Column';
 
@@ -7,11 +7,19 @@ export default function KanbanBoard(){
     const [completed, setCompleted] = useState([]);
     const [incomplete, setIncomplete] = useState([]);
     // const [inprogress, setInprogress] = useState([]);
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(response => response.json())
+        .then(json => console.log(json))
+            setCompleted(json.filter((task) => task.completed));
+            setIncomplete(json.filter((task) => !task.completed));
+    }, []);
+    
 
 
 
     return (
-        <DragDropContext>
+        <DragDropContext onDragEnd={handleDradEnd}>
             <h2 style={{ textAlign: "center"}}>PROGRESS BOARD</h2>
 
             <div style={{display: 'flex', justifyContent: 'space-between', alignContent: 'center', flexDirection:'row'}}>
