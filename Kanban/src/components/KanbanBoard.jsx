563e9af7c1febfc95caa012ca2d41b553602b90e
Sthabiso <sthabiso@aims.ac.za>
2024-02-23 17:35:15 +0200

    Created two functions, one for finding a task by id and the other for removing

--- Kanban/src/components/KanbanBoard.jsx
+++ Kanban/src/components/KanbanBoard.jsx
@@ -15,11 +15,38 @@ export default function KanbanBoard(){
             setIncomplete(json.filter((task) => !task.completed));
     }, []);
     
+    const handleDragEnd = (result) => {
+        const {destination, source, draggableId} = result;
+
+        if (source.droppableId == destination.droppableId) return;
+
+        //Remove from source array
+
+        if (source.droppableId == 2) {
+            setCompleted(removeItemById(draggableId, completed));
+        } else {
+            setIncomplete(removeItemById(draggableId, incomplete));
+        }
+
+        //Get item
+
+        const task = findItemById(draggableId, [...incomplete, ...completed]);
+
+        //Add item
+    };
+
+    function findItemById(id, array){
+        return array.find((item) => item.id == id);
+    }
+
+    function removeItemById(id, array){
+        return array.filter((item) => item.id != id);
+    }
 
 
 
     return (
-        <DragDropContext onDragEnd={handleDradEnd}>
+        <DragDropContext onDragEnd={handleDragEnd}>
             <h2 style={{ textAlign: "center"}}>PROGRESS BOARD</h2>
 
             <div style={{display: 'flex', justifyContent: 'space-between', alignContent: 'center', flexDirection:'row'}}>
